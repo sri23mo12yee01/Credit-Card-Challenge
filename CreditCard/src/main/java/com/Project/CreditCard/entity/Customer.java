3f83d8b9ee5f41eed0025bb31d2f8758781da4bf
Srimoyee Dutta <79530134+sri23mo12yee01@users.noreply.github.com>
2023-08-14 09:02:14 +0000

    Updated Customer.java

--- CreditCard/src/main/java/com/Project/CreditCard/entity/Customer.java
+++ CreditCard/src/main/java/com/Project/CreditCard/entity/Customer.java
@@ -1,50 +1,34 @@
 package com.Project.CreditCard.entity;
 
-import org.bson.types.ObjectId;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
 
-@Document("customers")
+
+@Document(collection="customers")
 public class Customer {
-
-
-//    public Customer(ObjectId id, int customerId, String firstName, String lastName, String country, char gender, String dob, String job) {
-//        this.id = id;
-//        this.customerId = customerId;
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//        this.country = country;
-//        this.gender = gender;
-//        this.dob = dob;
-//        this.job = job;
-//    }
-
     @Id
-    private ObjectId id;
-    private int customerId;
+    private String id;
     private String firstName;
     private String lastName;
-    private String country;
-    private char gender;
-    @Field("DateOfBirth")
-    private String dob;
-    private String job;
+    private String gender;
+    private String profession;
+    private Address address;
+ public Customer(String _id, String _firstName, String _lastName, String _gender, String _profession, Address _address) {
+        this.id = _id;
+        this.firstName = _firstName;
+        this.lastName = _lastName;
+        this.gender = _gender;
+        this.profession = _profession;
+        this.address = _address;
+    }
 
-    public ObjectId getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(ObjectId _id) {
+    public void setId(String _id) {
         this.id = _id;
-    }
-
-    public int getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(int _customerId) {
-        this.customerId = _customerId;
     }
 
     public String getFirstName() {
@@ -63,35 +47,28 @@ public class Customer {
         this.lastName = _lastName;
     }
 
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String _country) {
-        this.country = _country;
-    }
-
-    public char getGender() {
+    public String getGender() {
         return gender;
     }
 
-    public void setGender(char _gender) {
+    public void setGender(String _gender) {
         this.gender = _gender;
     }
 
-    public String getDob() {
-        return dob;
+    public String getProfession() {
+        return profession;
     }
 
-    public void setDob(String _dob) {
-        this.dob = _dob;
+    public void setProfession(String _profession) {
+        this.profession = _profession;
     }
 
-    public String getJob() {
-        return job;
+    public Address getAddress() {
+        return address;
     }
 
-    public void setJob(String _job) {
-        this.job = _job;
+    public void setAddress(Address _address) {
+        this.address = _address;
     }
+
 }
