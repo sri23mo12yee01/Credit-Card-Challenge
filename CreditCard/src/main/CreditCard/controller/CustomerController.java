ecf913528436222eb3fa06acf09e6d403a387bf0
Srimoyee Dutta <79530134+sri23mo12yee01@users.noreply.github.com>
2023-08-14 17:04:32 +0000

    Updated CustomerController.java

--- CreditCard/src/main/CreditCard/controller/CustomerController.java
+++ CreditCard/src/main/CreditCard/controller/CustomerController.java
@@ -1,9 +1,11 @@
 package com.Project.CreditCard.controller;
 
+import com.Project.CreditCard.entity.Customer;
 import com.Project.CreditCard.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +14,28 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
+    private final CustomerService customerService;
     @Autowired
-    private CustomerService service;
+    public CustomerController(CustomerService _customerService)
+    {
+        this.customerService=_customerService;
+    }
+
+    @GetMapping("/gender/{gender}")
+    public ResponseEntity<List<Customer>> getCustomersByGender(@PathVariable String gender)
+    {
+        List<Customer> customers = customerService.getCustomerByGender(gender);
+        return ResponseEntity.ok(customers);
+    }
+
+    @GetMapping("/profession/{profession}")
+    public ResponseEntity<List<Customer>> getCustomersByProfession(@PathVariable String profession)
+    {
+        List<Customer> customers=customerService.getCustomerByProfession(profession);
+        return ResponseEntity.ok(customers);
+    }
+
+
 
 
 
